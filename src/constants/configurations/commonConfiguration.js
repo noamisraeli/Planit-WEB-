cee93ac6120283cb5014b5d53fc614daf20cdb63
Noam Israeli <noamisraeli97@gmail.com>
2019-01-04 18:30:47 +0200

    Add gantt-view-gantt const

--- src/constants/configurations/commonConfiguration.js
+++ src/constants/configurations/commonConfiguration.js
@@ -3,3 +3,4 @@ export const END_TIME = "End time";
 export const WORKSPACE = "workspace";
 export const JOB = "job";
 export const QUEUE = "queue";
+export const GANTT_VIEW_GANTT = "gantt-view-gantt";
