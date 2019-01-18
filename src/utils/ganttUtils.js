653eacbcc813be3f24ff00b6641eaf77c8b01f9e
Noam Israeli <noamisraeli97@gmail.com>
2019-01-18 22:32:47 +0200

    Add toString argument

--- src/utils/ganttUtils.js
+++ src/utils/ganttUtils.js
@@ -4,8 +4,9 @@ export function getHourAsPixels(date1, date2, hourAsPixel){
     return Math.abs(date1 - date2) / 36e5 * hourAsPixel
 }
 
-export function getPixelsAsHour(length, startTime, hourAsPixel){
-    return new Date(startTime.getTime() + length / hourAsPixel * 36e5)
+export function getPixelsAsHour(length, startTime, hourAsPixel, asString){
+    const date = new Date(startTime.getTime() + length / hourAsPixel * 36e5)
+    return asString ? date.toLocaleString() : date
 
 }
 
