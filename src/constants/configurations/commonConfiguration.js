cad12f431c11310633130ebae1f0c6014cff142f
Noam Israeli <noamisraeli97@gmail.com>
2019-02-01 19:36:52 +0200

    Add gantt job const

--- src/constants/configurations/commonConfiguration.js
+++ src/constants/configurations/commonConfiguration.js
@@ -2,5 +2,7 @@ export const START_TIME = "Start time";
 export const END_TIME = "End time";
 export const WORKSPACE = "workspace";
 export const JOB = "job";
+export const GANTT_JOB = "GANTT_JOB";
+export const TABLE_JOB = "TABLE_JOB";
 export const QUEUE = "queue";
 export const GANTT_VIEW_GANTT = "gantt-view-gantt";
